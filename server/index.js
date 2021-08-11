4caa6df3a5da8be49623169cf7ad528cb2392269
nreynoldson <nicoleareynoldson@gmail.com>
2021-08-10 20:25:49 -0700

    initial code for nutrition facts grabber

---
+++ server/index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+const path = require('path');
+var cors = require('cors');
+app.use(cors());
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get('/api', async function(req, res){
+    var item = req.query.item;
+    var qty = req.query.qty;
+    try{
+        var response = await processNutritionalData(item, qty);
+        res.status(200).json(response);
+    }
+    catch(error){
+        res.status(error.response.status).send(error.message);
+    }
+    
+});
+
+async function processNutritionalData(query, qty){
+    var data = await getIngredientData(query);
+    var ing = data.foods[0];
+    var servingGrams = ing.serving_weight_grams;
+
+    var response = {};
+    response.item = ing.food_name;
+    
+    if(!qty)
+        qty = 100;
+
+    response.calories = (ing.nf_calories / servingGrams * qty).toFixed(2);
+    response.protein = (ing.nf_protein / servingGrams * qty).toFixed(2);
+    response.fat = (ing.nf_total_fat / servingGrams * qty).toFixed(2);
+    response.carbs = (ing.nf_total_carbohydrate / servingGrams * qty).toFixed(2);
+
+    return response;
+}
+    
+
+async function getIngredientData(query){
+    var url = "https://trackapi.nutritionix.com/v2/natural/nutrients/"
+    try {
+		const {data} = await axios.post(url,{"query": query},
+            { headers: {
+                'x-app-id': '3b9b1a3e',
+                'x-app-key': 'aff3ef846160d7102a314cec6b28d81e',
+                "Content-Type": "application/json"
+            }});
+		return data;
+	} catch (error) {
+		throw error;
+	}
+}
+
+process.on('uncaughtException', function (error) {
+    console.log(error.stack);
+ });
+
+const PORT = process.env.PORT || 8080;
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+
